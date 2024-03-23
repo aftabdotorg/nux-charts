1bafea14301d8da2f2fab94aa5689e644757c047
aftabdotorg <aft4b.akhand@gmail.com>
2024-03-23 18:14:38 +0530

    Added labels for ratings and customized datasets

--- pages/index.vue
+++ pages/index.vue
@@ -35,6 +35,9 @@ const chartData = computed((): ChartData<"bar"> => {
             {
                 label: "IMDb",
                 backgroundColor: ["#c82834"],
+                borderColor: "yellow",
+                borderWidth: 3,
+                borderRadius: {topLeft: 10},
                 data: movies.value.map(movie => movie.rating),
             },
             {
@@ -47,42 +50,9 @@ const chartData = computed((): ChartData<"bar"> => {
 })
 
 
-// const chartData = ref<ChartData<"bar">>({
-//     labels: [
-//         "Spider-Man (2002)",
-//         "Spider-Man 2 (2004)",
-//         "Spider-Man 3 (2007)",
-//         "The Amazing Spider-Man (2012)",
-//         "The Amazing Spider-Man 2 (2014)",
-//         "Spider-Man: Homecoming (2017)",
-//         "Spider-Man: Far From Home (2019)",
-//         "Spider-Man: No Way Home (2021)",
-//         "Spider-Man: Into the Spider-Verse (2018)",
-//         "Spider-Man: Across the Spider-Verse (2023)",
-//     ],
-//     datasets: [
-//         {
-//             backgroundColor: ["#c82834", "#244771"],
-//             data: [7.3, 7.3, 6.2, 6.9, 6.6, 7.4, 7.5, 8.0, 8.4, 8.9]
-//         }
-//     ]
-// });
-
-
-
 </script>
 <template>
-    <Bar
-     v-if="movies.length"
-     :data="chartData" 
-     :options="{
-        plugins: {
-            legend: {
-                display: false
-            }
-        }
-    }" 
-    />
+    <Bar v-if="movies.length" :data="chartData" />
 </template>
 
 <style>
